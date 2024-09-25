5c3e03a6cdb5ec164eb1cd207ac4819d3ba626cd
Aleeez <alice.gergely@gmail.com>
2024-09-25 22:46:57 +0300

    setting up db connection

--- bsz-backend/src/main/java/org/dn/team/basszhattyuk/Main.java
+++ bsz-backend/src/main/java/org/dn/team/basszhattyuk/Main.java
@@ -1,11 +1,18 @@
 package org.dn.team.basszhattyuk;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(Main.class, args);
     }
 }
