65e06bdcac58811bda805c382eafed35a62bcf12
Aleeez <alice.gergely@gmail.com>
2024-11-01 13:59:55 +0200

    order and item type: frontend

---
+++ bsz-backend/src/main/java/org/dn/team/basszhattyuk/dto/incoming/OrderInDTO.java
@@ -0,0 +1,9 @@
+package org.dn.team.basszhattyuk.dto.incoming;
+
+import org.dn.team.basszhattyuk.model.ItemModel;
+
+public class OrderInDTO {
+
+    private ItemModel[] items;
+    private String note;
+}
---
+++ bsz-backend/src/main/java/org/dn/team/basszhattyuk/dto/outgoing/OrderOutDTO.java
@@ -0,0 +1,9 @@
+package org.dn.team.basszhattyuk.dto.outgoing;
+
+import org.dn.team.basszhattyuk.model.ItemModel;
+
+public class OrderOutDTO {
+    private ItemModel[] items;
+    private Integer totalPrice;
+    private String note;
+}
