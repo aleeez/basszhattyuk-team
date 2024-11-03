04772f3e5fc859fe11d43c14a266a6c078282607
Aleeez <alice.gergely@gmail.com>
2024-11-03 19:53:50 +0200

    checkbox fixed

--- bsz-frontend/src/dto/PlayerType.tsx
+++ bsz-frontend/src/dto/PlayerType.tsx
@@ -1,13 +1,13 @@
 export interface PlayerType {
     id?: number,
-    lastname: string,
-    firstname: string,
-    phone: string,
+    lastName: string,
+    firstName: string,
+    phoneNr: string,
     email: string,
     seriaNr: string,
     fbLink: string,
     external: boolean,
     kmdszID: string,
     passPic: string,
-    studPic: string
+    studIDPic: string
 }
